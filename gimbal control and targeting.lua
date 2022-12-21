9e32c37ba992cc6c379c3aa06a0473879db17a35
maxBHarris <107074618+maxBHarris@users.noreply.github.com>
2022-12-21 09:54:36 -0500

    Update gimbal control and targeting.lua
    Update to allow for finite time horizon location prediction and targeting changes.

--- gimbal control and targeting.lua
+++ gimbal control and targeting.lua
@@ -4,12 +4,14 @@ local PAN_MAX_PWM = 2110
 local PAN_MIN_PWM = 890
 local PAN_MAX_ANGLE = 179.99
 local PAN_MIN_ANGLE = -180.0
-local PAN_OFFSET = -219
+local PAN_OFFSET = -189 --add to go left
 local TILT_SERVO_PORT = 6
 local TILT_MAX_PWM = 2300
 local TILT_MIN_PWM = 800
 local TILT_MAX_ANGLE = 50.0
 local TILT_MIN_ANGLE = -90.0
+local CURR_PAN_PWM = 0
+local CURR_TILT_PWM = 0
 
 function make(xval, yval, zval)
     return {x=xval, y=yval, z=zval}
@@ -104,8 +106,24 @@ function tilt_gimbal(angle)
     local pwm_range = TILT_MAX_PWM - TILT_MIN_PWM
     local angle_range = TILT_MAX_ANGLE - TILT_MIN_ANGLE
 
-    local pwm_output = pwm_range*(normalize(angle, TILT_MIN_ANGLE, TILT_MAX_ANGLE))+TILT_MIN_PWM
+    local pwm_goal = pwm_range*(normalize(angle, TILT_MIN_ANGLE, TILT_MAX_ANGLE))+TILT_MIN_PWM
+    local pwm_output = pwm_goal
+    if math.abs(CURR_TILT_PWM-pwm_goal)> 10 then
+      if pwm_goal > CURR_TILT_PWM then
+        pwm_goal = CURR_TILT_PWM + 10
+         CURR_TILT_PWM = pwm_output
+        SRV_Channels:set_output_pwm_chan(TILT_SERVO_PORT, math.floor(pwm_output))
+        return
+      end
+      if pwm_goal < CURR_TILT_PWM then
+        pwm_goal = CURR_TILT_PWM - 10
+         CURR_TILT_PWM = pwm_output
+        SRV_Channels:set_output_pwm_chan(TILT_SERVO_PORT, math.floor(pwm_output))
+        return
+        end
+    end
     --gcs:send_text(6, string.format("Tilt Angle, PWM: %.1f, %.1f", angle, pwm_output))
+    CURR_TILT_PWM = pwm_output
     SRV_Channels:set_output_pwm_chan(TILT_SERVO_PORT, math.floor(pwm_output))
 end
 
@@ -128,7 +146,25 @@ function pan_gimbal(angle)
     end
 
     local pwm_range = PAN_MAX_PWM - PAN_MIN_PWM
-    local pwm_output = pwm_range*(normalize(angle, PAN_MIN_ANGLE, PAN_MAX_ANGLE))+PAN_MIN_PWM
+    local pwm_goal = pwm_range*(normalize(angle, PAN_MIN_ANGLE, PAN_MAX_ANGLE))+PAN_MIN_PWM
+    local pwm_output = pwm_goal
+    if math.abs(CURR_PAN_PWM-pwm_goal)> 10 then
+      if pwm_goal > CURR_PAN_PWM then
+        pwm_output = CURR_PAN_PWM + 10
+        CURR_PAN_PWM = pwm_output
+        SRV_Channels:set_output_pwm_chan(PAN_SERVO_PORT, math.floor(pwm_output))
+        return
+      end
+      if pwm_goal < CURR_PAN_PWM then
+        pwm_output = CURR_PAN_PWM - 10
+        CURR_PAN_PWM = pwm_output
+        SRV_Channels:set_output_pwm_chan(PAN_SERVO_PORT, math.floor(pwm_output))
+        return
+        end
+    end
+    --gcs:send_text(6, string.format("Tilt Angle, PWM: %.1f, %.1f", angle, pwm_output))
+    --SRV_Channels:set_output_pwm_chan(TILT_SERVO_PORT, math.floor(pwm_output))
+    CURR_PAN_PWM = pwm_output
     SRV_Channels:set_output_pwm_chan(PAN_SERVO_PORT, math.floor(pwm_output))
 end
 
@@ -182,13 +218,77 @@ function calculate_pan_and_tilt(roll, pitch, yaw, target_lat, target_lng, target
     end
     --gcs:send_text(6, string.format("Rel Alt, Pan_Aim_Z: (%.1f, %.1f)", rel_alt, pan_aim.z))
     gcs:send_text(6, string.format("Tilt AIM Angle: %.1f", tilt_aim_angle))
-    pan_gimbal(-1*(pan_aim_angle + math.deg(yaw) + PAN_OFFSET))
-    tilt_gimbal(-1*(tilt_aim_angle))
+    pan_gimbal((pan_aim_angle + math.deg(yaw) + PAN_OFFSET))
+    tilt_gimbal((tilt_aim_angle))
 end
 
+function finite_horizon_position(curr_velocity, curr_position)
+  local distance_traveled_north = curr_velocity[1]*5
+  local new_lat = curr_position:lat() + (distance_traveled_north*360/(2*math.pi*6371000))
+  local distance_traveled_east = curr_velocity[2]*5
+  local new_lng = curr_position:lng() + (distance_traveled_north*360/(2*math.pi*6371000))
+  find_target_theory(new_lat, new_lng) 
+  end
+  
+function find_target_theory(theory_lat, theory_long)
+  
+  local input = io.open("/APM/scripts/GCS_locations.txt", "r")
+  local counter = 0;
+  io.input(input)
+  PAN_SERVO_PORT = io.read("*number")
+  PAN_MIN_PWM = io.read("*number")
+  PAN_MAX_PWM = io.read("*number")
+  TILT_SERVO_PORT = io.read("*number")
+  TILT_MIN_PWM = io.read("*number")
+  TILT_MAX_PWM = io.read("*number")
+  
+  num_GCS = io.read("*number")
+  min_dist = 1000000000000000000
+  target_lat = 0
+  target_lng =0
+  target_alt = 0
+  GCS_target = 0
+  for i=1, num_GCS do
+    local GCS_lat, GCS_lng, GCS_alt = io.read("*number", "*number", "*number")
+    --gcs:send_text(0, string.format("GCS loc: (%.2f, %.2f, %.2f)", GCS_lat, GCS_lng, GCS_alt))
+    local dist = haversine(((theory_lat)/10000000), ((theory_long)/10000000), GCS_lat, GCS_lng)
+    if (dist<min_dist)
+    then
+      min_dist = dist
+      local theory_target_alt = GCS_alt*1000
+      local theory_target_lat = GCS_lat*10000000
+      local theory_target_lng = GCS_lng*10000000
+      local theory_GCS_target = i
+    end
+    if (theory_GCS_target != GCS_target)
+    then
+      counter = counter + 1;
+      if (counter == 5)
+      then
+        GCS_target = theory_GCS_target
+        target_alt = theory_target_alt
+        target_lat = theory_target_lat
+        target_lng = theory_target_lng
+        counter = 0
+      end
+        
+    end
+    
+  end
+end
+function find_obstructions(curr_pos, target_lat, target_lng, target_alt)
+  local vector_to_target = vector.new(((curr_pos:lat()/10000000)-target_lat), (((curr_pos:lng())/10000000)-target_lng), ((curr_pos:alt/1000)-target_alt))
+  end
 function find_target(curr_pos)
   local input = io.open("/APM/scripts/GCS_locations.txt", "r")
   io.input(input)
+  PAN_SERVO_PORT = io.read("*number")
+  PAN_MIN_PWM = io.read("*number")
+  PAN_MAX_PWM = io.read("*number")
+  TILT_SERVO_PORT = io.read("*number")
+  TILT_MIN_PWM = io.read("*number")
+  TILT_MAX_PWM = io.read("*number")
+  
   num_GCS = io.read("*number")
   min_dist = 1000000000000000000
   target_lat = 0
@@ -202,7 +302,7 @@ function find_target(curr_pos)
     if (dist<min_dist)
     then
       min_dist = dist
-      target_alt = GCS_alt*100
+      target_alt = GCS_alt*1000
       target_lat = GCS_lat*10000000
       target_lng = GCS_lng*10000000
       GCS_target = i
@@ -218,6 +318,7 @@ function update() -- this is the loop which periodically runs
     --gcs:send_text(0, "running")
     local curr_pos = ahrs:get_position()    -- fetch the current location of the vehicle
     local home = ahrs:get_home()            -- fetch the origin position of the vehicle
+    local curr_velocity = ahrs:groundspeed_vector()
 
     --gcs:send_text(6, tostring(vector_from_home))
      --vector needed to verify EKF origin is set. EKF origin is needed to get alt
@@ -238,15 +339,17 @@ function update() -- this is the loop which periodically runs
     if home and curr_pos and arming:is_armed() then
         
         find_target(curr_pos)
+        
         gcs:send_text(0, string.format("Curr Pos: (%.5f, %.5f, %.2f)", curr_pos:lat(), curr_pos:lng(), curr_pos:alt()))
         gcs:send_text(0, string.format("Target: %.1f", GCS_target))
         gcs:send_text(0, string.format("Target: (%.4f, %.4f, %.4f)", target_lat, target_lng, target_alt))
         --gcs:send_text(6, string.format("Target location: (%.2f, %.2f, %.2f)", home:lat, home:lng, home:alt))
-        calculate_pan_and_tilt(roll, pitch, yaw, target_lat,target_lng,target_alt, curr_pos)
+        calculate_pan_and_tilt(roll, pitch, yaw, target_lat, target_lng, target_alt, curr_pos)
         --gcs:send_text(0, "calculated")
+        finite_horizon_position(curr_velocity, curr_pos)
     end
 
-    return update, 100 -- reschedules the loop
+    return update, 5 -- reschedules the loop
   --end
 end
 
